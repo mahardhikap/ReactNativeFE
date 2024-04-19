daaf28aa5bc5897c9c2860dd3157f54d77272a36
Mahardhika Pratama <putrad578@gmail.com>
2024-04-19 10:26:37 +0700

    integration dispatch bookmarkscreen and styling

--- src/screens/BookmarkScreen.js
+++ src/screens/BookmarkScreen.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text, View, Button, ScrollView, StyleSheet} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {removeBookmark} from '../actions/articleAction';
 
-const BookmarkScreen = () => {
+export default function BookmarkScreen() {
+  const dispatch = useDispatch();
+  const bookmarks = useSelector(state => state.articles);
+
+  const bookmarkedArticles = bookmarks?.filter(bookmark => bookmark.bookmarked);
+
+  const handleRemoveBookmark = id => {
+    dispatch(removeBookmark(id));
+  };
+
   return (
-    <View>
-      <View>
-        <Text>Bookmark</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        {bookmarkedArticles?.map(bookmark => (
+          <View key={bookmark.id} style={styles.articleContainer}>
+            <Text style={styles.title}>{bookmark.title}</Text>
+            <Text>{bookmark.body}</Text>
+            <Text>Created by: {bookmark.userId}</Text>
+            <Button
+              title="Remove Bookmark"
+              onPress={() => handleRemoveBookmark(bookmark.id)}
+              color="red"
+            />
+          </View>
+        ))}
       </View>
-    </View>
+    </ScrollView>
   );
-};
+}
 
-export default BookmarkScreen;
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
+  articleContainer: {
+    marginBottom: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 5,
+  },
+});
