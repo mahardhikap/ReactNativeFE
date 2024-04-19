f8216c8a747fa3287dca49ed288b8290357fec12
Mahardhika Pratama <putrad578@gmail.com>
2024-04-19 10:23:34 +0700

    integration dispatch homescreen and styling

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {Text, View, Button, ScrollView, StyleSheet} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchArticles, toggleBookmark} from '../actions/articleAction';
 
-const HomeScreen = () => {
+export default function HomeScreen() {
+  const dispatch = useDispatch();
+  const articles = useSelector(state => state.articles);
+
+  useEffect(() => {
+    dispatch(fetchArticles());
+  }, []);
+
+  const handleToggleBookmark = id => {
+    dispatch(toggleBookmark(id));
+  };
+
   return (
-    <View>
-      <View>
-        <Text>Home</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        {articles?.map(article => (
+          <View key={article.id} style={styles.articleContainer}>
+            <Text style={styles.title}>{article.title}</Text>
+            <Text>{article.body}</Text>
+            <Text>Created by: {article.userId}</Text>
+            <Button
+              title={article.bookmarked ? 'Unbookmark' : 'Bookmark'}
+              onPress={() => handleToggleBookmark(article.id)}
+              color={article.bookmarked ? 'red' : 'green'}
+            />
+          </View>
+        ))}
       </View>
-    </View>
+    </ScrollView>
   );
-};
+}
 
-export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
+  articleContainer: {
+    marginBottom: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 5,
+  },
+});
